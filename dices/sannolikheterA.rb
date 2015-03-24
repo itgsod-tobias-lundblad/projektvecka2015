7a6c2be49a44e3aa7d05b70667bde5694921b9b4
itgsod-tobias-lundblad <tobias.lundblad@elev.it-gymnasiet.se>
2015-03-24 14:31:59 +0100

    Klar med uppgift 1c

--- dices/sannolikheterA.rb
+++ dices/sannolikheterA.rb
@@ -6,4 +6,4 @@ puts "hur många olika utfall vill du få?"
 
 b=gets.to_i
 
-puts "Det är #{((b/a.to_f)*100).round}% chans att få det utfallet som du vill få"
+puts "Det är #{((b/a.to_f)*100).round}% chans att få det utfallet som du vill ha"
